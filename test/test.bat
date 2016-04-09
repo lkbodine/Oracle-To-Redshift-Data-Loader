d6c9f468fa0a698d7592cd9b72053300ff846b5c
Alex Buzunov <alexbuzunov@gmail.com>
2016-04-08 23:04:34 -0400

    Update test.bat

--- test/test.bat
+++ test/test.bat
@@ -1,8 +1,12 @@
-set AWS_ACCESS_KEY_ID=<you access key>
-set AWS_SECRET_ACCESS_KEY=<you secret key>
-set ORACLE_LOGIN=tiger/scott@orcl
-set ORACLE_CLIENT_HOME=C:\\app\\oracle12\\product\\12.1.0\\dbhome_1
-set REDSHIFT_CONNECT_STRING="dbname='***' port='5439' user='***' password='***' host='mycluster.***.redshift.amazonaws.com'"  
+  set AWS_ACCESS_KEY_ID=<you access key>
+  set AWS_SECRET_ACCESS_KEY=<you secret key>
+
+  set ORACLE_LOGIN=tiger/scott@orcl
+  set ORACLE_CLIENT_HOME=C:\app\oracle12\product\12.1.0\dbhome_1
+  
+  set NLS_DATE_FORMAT="MM/DD/YYYY HH12:MI:SS"
+  set NLS_TIMESTAMP_FORMAT="MM/DD/YYYY HH12:MI:SS.FF"
+  set NLS_TIMESTAMP_TZ_FORMAT="MM/DD/YYYY HH12:MI:SS.FF TZH:TZM"
 
 
 dist-64bit\oracle_to_redshift_loader.exe ^
