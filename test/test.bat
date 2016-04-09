6a9c6ebd5923db1489d73f991fbdb258684d0409
Alex Buzunov <alexbuzunov@gmail.com>
2016-04-08 22:16:19 -0400

    Update test.bat

--- test/test.bat
+++ test/test.bat
@@ -1,3 +1,10 @@
+set AWS_ACCESS_KEY_ID=<you access key>
+set AWS_SECRET_ACCESS_KEY=<you secret key>
+set ORACLE_LOGIN=tiger/scott@orcl
+set ORACLE_CLIENT_HOME=C:\\app\\oracle12\\product\\12.1.0\\dbhome_1
+set REDSHIFT_CONNECT_STRING="dbname='***' port='5439' user='***' password='***' host='mycluster.***.redshift.amazonaws.com'"  
+
+
 dist-64bit\oracle_to_redshift_loader.exe ^
 -q table_query.sql ^
 -d "," ^
