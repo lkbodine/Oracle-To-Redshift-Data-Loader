15c0cdb0c7d7cc466d4282c37bdab2f167774962
alex buzunov <alexbuzunov@gmail.com>
2016-04-14 20:19:07 -0400

    extractor

--- dist-64bit/include/loader.py
+++ dist-64bit/include/loader.py
@@ -5,7 +5,7 @@ REDSHIFT_CONNECT_STRING = g['REDSHIFT_CONNECT_STRING']
 AWS_ACCESS_KEY_ID = g['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = g['AWS_SECRET_ACCESS_KEY']
 
-def load(location, globals):
+def load(location):
 	start_time = time.time()
 	fn='s3://%s' % location
 	conn_string = REDSHIFT_CONNECT_STRING.strip().strip('"')	
@@ -19,7 +19,7 @@ def load(location, globals):
 		ignoreheader='IGNOREHEADER %s' % opt.red_ignoreheader
 	timeformat=''
 	if opt.red_timeformat:
-		timeformat=" dateformat 'auto' " #"timeformat '%s'" % opt.red_timeformat
+		#timeformat=" dateformat 'auto' "
 		timeformat=" timeformat '%s'" % opt.red_timeformat.strip().strip("'")
 	sql="""
 COPY %s FROM '%s' 
