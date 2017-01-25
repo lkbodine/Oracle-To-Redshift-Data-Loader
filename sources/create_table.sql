716328e5de439875f5a7d6ebdc8f2c7350eadc21
Alex Buzunov <abuzunov@kcg.com>
2017-01-25 09:17:50 -0500

    cleanup

--- sources/create_table.sql
+++
@@ -1,7 +0,0 @@
-drop table test;
-create table crime_test (
-Incident_ID VARCHAR2(20),CR_Number VARCHAR2(20),Dispatch_Date_Time TIMESTAMP,Class VARCHAR2(10) ,Class_Description VARCHAR2(100),
-Police_District_Name VARCHAR2(40),Block_Address VARCHAR2(100),
-City VARCHAR2(40),State VARCHAR2(8),Zip_Code VARCHAR2(10),
-Agency VARCHAR2(40),Place VARCHAR2(40),Sector VARCHAR2(10) ,Beat VARCHAR2(10),PRA VARCHAR2(10),Start_Date_Time TIMESTAMP,End_Date_Time TIMESTAMP,
-Latitude VARCHAR2(20),Longitude VARCHAR2(20),Police_District_Number VARCHAR2(50),Location VARCHAR2(80),Address_Number VARCHAR2(30));
--- sources/table_query.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM c##test.crime_test;
